c1f9378a8af0c66e88c597472d45f2212d9643ef
yanaph <fan37yana@gmail.com>
2022-01-26 20:01:42 +0200

    first homework

---
+++ src/ua/fan/hw1/Task3.java
@@ -0,0 +1,10 @@
+package ua.fan.hw1;
+
+public class Task3 {
+    public static void main(String[] args) {
+        System.out.println("----> Task 3:");
+        for (int s = 0, v = 5; s <= 10; s++, v = v + 2) {
+            System.out.println("Step " + s + ": " + "value " + v);
+        }
+    }
+}
