18f310b13d6b48f99732e7a934fafa48aabb922e
yanaph <fan37yana@gmail.com>
2022-02-18 00:49:27 +0200

    hw9 implementation

---
+++ src/main/java/ua/fan/hw1/Task1.java
@@ -0,0 +1,9 @@
+package ua.fan.hw1;
+
+public class Task1 {
+    public static void main(String[] args) {
+        System.out.println("----> Task 1:");
+        String surname = "Fan", name = "Zieu Lin";
+        System.out.println(surname + " " + name + "\n");
+    }
+}
