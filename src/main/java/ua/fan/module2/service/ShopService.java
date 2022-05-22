3c16be0db6f7c7d79aa01c1e30c247cac73a727a
yanaph <fan37yana@gmail.com>
2022-05-22 19:05:10 +0200

    module 2 removing the block comments

--- src/main/java/ua/fan/module2/service/ShopService.java
+++ src/main/java/ua/fan/module2/service/ShopService.java
@@ -48,27 +48,27 @@ public class ShopService {
 
     public void analyticRequestsResult(){
         final AnalyticalRequests analyticalRequests = new AnalyticalRequests();
-//        System.out.println("\n---> Number of items sold by category: \n" +
-//                "Televisions: " + analyticalRequests.soldAmountSortedByCategory(invoices, "Television") +
-//                "\nTelephones: " + analyticalRequests.soldAmountSortedByCategory(invoices, "Telephone"));
-//
-//        System.out.println("\n---> Customer info of invoice with minimum total price: \n" +
-//                "Total: " + analyticalRequests.minSumInvoiceInfo(invoices).getTotalPrice() +
-//                "\n Customer info: \n" + analyticalRequests.minSumInvoiceInfo(invoices).getCustomer());
-//
-//        System.out.println("\n---> Total price of all invoices: \n" +
-//                "Total: " + analyticalRequests.getTotalPriceOfAllInvoices(invoices));
-//
-//        System.out.println("\n---> Number of invoices with retail type:\n" +
-//                analyticalRequests.getRetailInvoicesNumber(invoices));
+        System.out.println("\n---> Number of items sold by category: \n" +
+                "Televisions: " + analyticalRequests.soldAmountSortedByCategory(invoices, "Television") +
+                "\nTelephones: " + analyticalRequests.soldAmountSortedByCategory(invoices, "Telephone"));
+
+        System.out.println("\n---> Customer info of invoice with minimum total price: \n" +
+                "Total: " + analyticalRequests.minSumInvoiceInfo(invoices).getTotalPrice() +
+                "\n Customer info: \n" + analyticalRequests.minSumInvoiceInfo(invoices).getCustomer());
+
+        System.out.println("\n---> Total price of all invoices: \n" +
+                "Total: " + analyticalRequests.getTotalPriceOfAllInvoices(invoices));
+
+        System.out.println("\n---> Number of invoices with retail type:\n" +
+                analyticalRequests.getRetailInvoicesNumber(invoices));
 
         System.out.println("\n---> Invoices that contain only one type of devices:\n" +
                 analyticalRequests.getOneDeviceTypeInvoices(invoices));
 
-//        System.out.println("\n---> Low age invoices:\n " +
-//                analyticalRequests.getLowAgeInvoices(invoices));
-//
-//         System.out.println("\n---> Sorting invoices by condition:\n " +
-//                analyticalRequests.sortInvoicesByCondition(invoices));
+        System.out.println("\n---> Low age invoices:\n " +
+                analyticalRequests.getLowAgeInvoices(invoices));
+
+         System.out.println("\n---> Sorting invoices by condition:\n " +
+                analyticalRequests.sortInvoicesByCondition(invoices));
     }
 }
