fb6fdcc2fa3c980740dbe625ac404fa8f333ad25
yanaph <fan37yana@gmail.com>
2022-05-27 13:11:33 +0200

    comments deleting

--- src/main/java/ua/fan/hw21/Main.java
+++ src/main/java/ua/fan/hw21/Main.java
@@ -6,9 +6,5 @@ import ua.fan.hw21.annotations.AnnotationService;
 public class Main {
     public static void main(String[] args) {
         AnnotationService.init();
-//        Box box = new Box();
-//        Box box2 = new Box();
-//        System.out.println(box);
-//        System.out.println(box2);
     }
 }
